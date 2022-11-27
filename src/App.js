428a4a8c05663c5d15762606a312f643a13f832e
rookieno <rhkdals3257@naver.com>
2022-11-27 20:15:38 +0900

    render when Create and copy

--- src/App.js
+++ src/App.js
@@ -41,17 +41,34 @@ function Article(props) {
   </article>
 }
 
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body)
+    }}>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"/></p>
+    </form>
+  </article>
+}
+
 function App() {
   // const _mode = useState("WELCOME");
   // const mode = _mode[0];
   // const setMode = _mode[1];
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:"html", body:"html is ..."},
     {id:2, title:"css", body:"css is ..."},
     {id:3, title:"js", body:"js is ..."}
-  ]
+  ]);
   let content = null;
   if(mode === "WELCOME"){
     content = <Article title="Welcome" body="Hello, WEB"></Article>
@@ -64,6 +81,13 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  }else if(mode === "CREATE"){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+    }}></Create>
   }
   return (
     <div>
@@ -75,6 +99,10 @@ function App() {
         setId(_id);
       }}></Nav>
     {content}
+    <a href="/create" onClick={event=>{
+      event.preventDefault();
+      setMode("CREATE");
+    }}>Create</a>
     </div>
   );
 }
